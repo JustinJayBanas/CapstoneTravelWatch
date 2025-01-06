71db633ae1a9e1a2d988a8abe7187bf030c1b6ff
JustinJay <120239545+JustinJayBanas@users.noreply.github.com>
2025-01-07 01:51:54 +0800

    e

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration
 define('DB_HOST', 'localhost'); // Usually 'localhost' for shared hosting
-define('DB_USER', 'root'); // Your database username
-define('DB_PASS', ''); // Your database password
-define('DB_NAME', 'travelwatch'); // Your database name
+define('DB_USER', 'u888530159_travelwatch'); // Your database username
+define('DB_PASS', 'Pa$sw0rd123!'); // Your database password
+define('DB_NAME', 'u888530159_travelwatch'); // Your database name
 
 // Other configurations can be added here
 ?>
